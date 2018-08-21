e4180010ca5798d2cb694ee904c30f622e7c7505
Zach Battleman <32079362+Zanolon@users.noreply.github.com>
2018-08-21 00:56:08 -0400

    Spaghetti Code
    Spaghetti Code

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
+function cardChanger(a){
+  var someVarName = a;
+  sessionStorage.setItem("Key", someVarName);
+}
+
+var card = sessionStorage.getItem("Key");
+
+document.getElementById("img").src="Assets/Cards/" +card+"/"+card+".png"
+document.getElementById("audio").src="Assets/Cards/" +card+"/"+card+".ogg"
+
+
 img = document.getElementById("img")
 audio = document.getElementById("audio")
 
 counter = 0
-
 function placeImage(e) {
   imgc = $("#img").clone().appendTo("body")[0]
   audioc=$("#audio").clone().appendTo("body")[0]
@@ -11,7 +21,8 @@ function placeImage(e) {
   imgc.style.top = (e.clientY - (img.height / 2)) + "px";
   audioc.play();
   counter++
-  console.log(counter)
 }
+
+
 
 document.addEventListener("click", placeImage)
