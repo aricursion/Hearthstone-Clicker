c9d0a0816035f69e29dfcf83fca080db5241087b
Zach Battleman <32079362+Zanolon@users.noreply.github.com>
2018-08-23 09:35:11 -0400

    Alphabetical list sorting and added new cards

--- js/home.js
+++ js/home.js
@@ -14,6 +14,29 @@ function SearchBar() {
     }
   }
 
+  function sortList() {
+    var list, i, switching, b, shouldSwitch;
+    list = document.getElementById("myUL");
+    switching = true;
+    while (switching) {
+      switching = false;
+      b = list.getElementsByTagName("li");
+      for (i = 0; i < (b.length - 1); i++) {
+        shouldSwitch = false;
+        if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          break;
+        }
+      }
+      if (shouldSwitch) {
+        b[i].parentNode.insertBefore(b[i + 1], b[i]);
+        switching = true;
+      }
+    }
+  }
+
+  sortList();
+
   /*document.getElementById('myInput').onkeypress = function(e){
       if (!e) e = window.event;
       var keyCode = e.keyCode || e.which;
